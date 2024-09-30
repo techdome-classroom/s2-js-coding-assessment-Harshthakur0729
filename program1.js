d01861c9617bc84bf8d180e0d61a250ed1645e75
Harshad Thakur <116425178+Harshthakur0729@users.noreply.github.com>
2024-09-30 14:47:53 +0530

    Update program1.js

--- program1.js
+++ program1.js
@@ -2,10 +2,42 @@
  * @param {string} s
  * @return {boolean}
  */
-var isValid = function(s) {
+var isValid = function(s)  {
+    let stack = [];
+    const map = {
+        ')': '(',
+        '}': '{',
+        ']': '['
+    };
+        for (let i = 0; i < s.length; i++) {
+        let char = s[i];
+        
+
+        if (map[char]) {
+
+            let topElement = stack.length ? stack.pop() : '#';
+
+            if (topElement !== map[char]) {
+                return false;
+            }
+        } else {
+
+            stack.push(char);
+        }
+    }
     
-};
+  
+    return stack.length === 0;
+}
+
+console.log(isValid("()"));       
+console.log(isValid("()[]{}"));   
+console.log(isValid("(]"));       
 
 module.exports = { isValid };
+
+
+
+
 
 
