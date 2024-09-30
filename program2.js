95dd2321c3a5f5d54bcfd44cab5fba8d3dc827a7
Harshad Thakur <116425178+Harshthakur0729@users.noreply.github.com>
2024-09-30 14:46:09 +0530

    Update program2.js

--- program2.js
+++ program2.js
@@ -3,7 +3,37 @@
  * @return {number}
  */
 var romanToInt = function(s) {
+    var isValid = function(s)  {
+    let stack = [];
+    const map = {
+        ')': '(',
+        '}': '{',
+        ']': '['
+    };
+        for (let i = 0; i < s.length; i++) {
+        let char = s[i];
+        
+
+        if (map[char]) {
+
+            let topElement = stack.length ? stack.pop() : '#';
+
+            if (topElement !== map[char]) {
+                return false;
+            }
+        } else {
+
+            stack.push(char);
+        }
+    }
     
+  
+    return stack.length === 0;
+}
+
+console.log(isValid("()"));       
+console.log(isValid("()[]{}"));   
+console.log(isValid("(]"));       
 };
 
 
